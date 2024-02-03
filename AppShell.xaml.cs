552386f4a2b1a522cf904e6913f6c516afe1a3af
Girish4489 <girishkumar4489@gmail.com>
2024-02-03 08:46:52 +0530

    Adding exit functionality

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace LoginMaui
+﻿using System.Diagnostics;
+
+namespace LoginMaui
 {
 	public partial class AppShell : Shell
 	{
@@ -7,5 +9,16 @@
 			InitializeComponent(); 
 		}
 
+		private async void OnExitMenuItemClicked(object sender, EventArgs e)
+		{
+			// Close the app			
+			bool answer = await DisplayAlert("Exit", "Do you want to exit from the app or stay?", "Exit", "Cancel");
+			if (answer)
+			{
+				//close the app
+				Application.Current!.Quit();
+				//System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
+			}
+		}
 	}
 }
